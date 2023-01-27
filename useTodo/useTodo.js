ecad02f87fb4c3ec5e1b2c933aba32fb11fb503b
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-01-27 12:36:02 -0600

    Adding folders

---
+++ useTodo/useTodo.js
@@ -0,0 +1,39 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+export const useTodo = () => {
+  const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+  };
+
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const onRemoveTodo = (id) => {
+    dispatch({ type: "[TODO] Remove Todo", payload: id });
+  };
+
+  const onToggleTodo = (id) => {
+    dispatch({ type: "[TODO] Change State Todo", payload: id });
+  };
+
+  return {
+    onNewTodo,
+    onRemoveTodo,
+    onToggleTodo,
+    todos,
+    todosCount: todos.length,
+    todosPendingCount: todos.filter((todo) => !todo.done).length,
+  };
+};
